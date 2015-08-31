8f959e4e1a8c4a6bd470b13bca82a999d209d002
Oliver Lee <oliver_lee@winmate.com.tw>
2015-08-31 16:46:28 +0800

    2015-08-31_012

--- js/jquery.twzipcode-1.7.1.js
+++ js/jquery.twzipcode-1.7.1.js
@@ -463,25 +463,25 @@
                 readonly = role.zipcode.data('readonly') || opts.readonly;
 
             // Elements create
+
+            $('<input/>')
+                .attr({'type': 'label', 'name': zipcodeName})
+                .prop('readonly', readonly)
+				.addClass('new_style')
+                .addClass(role.zipcode.data('style') || (undefined !== opts.css[2] ? opts.css[2] : ''))
+                .appendTo(role.zipcode.length ? role.zipcode : container);
+				
             $('<select/>')
                 .attr('name', countyName)
                 .addClass(role.county.data('style') || (undefined !== opts.css[0] ? opts.css[0] : ''))
                 .addClass('btn-sm')
-                .addClass('btn_new')
                 .appendTo(role.county.length ? role.county : container);
 
             $('<select/>')
                 .attr('name', districtName)
                 .addClass(role.district.data('style') || (undefined !== opts.css[1] ? opts.css[1] : ''))
                 .addClass('btn-sm')
-                .addClass('btn_new')
                 .appendTo(role.district.length ? role.district : container);
-
-            $('<input/>')
-                .attr({'type': 'text', 'name': zipcodeName})
-                .prop('readonly', readonly)
-                .addClass(role.zipcode.data('style') || (undefined !== opts.css[2] ? opts.css[2] : ''))
-                .appendTo(role.zipcode.length ? role.zipcode : container);
 
             self.wrap = {
                 'county': container.find('select[name="' + countyName + '"]:first'),
